988e1cf6db234a7f61d9a2e1cedf17cc78d8ea69
Erik Tangvik <erikjt@outlook.com>
2019-10-22 09:30:51 -0500

    Started generate password function

--- main.js
+++ main.js
@@ -1,3 +1,30 @@
+// Dom elements
+const resultEl = document.getElementById("result");
+const lengthEl = document.getElementById("length");
+const uppercaseEl = document.getElementById("upper");
+const lowercaseEl = document.getElementById("lowercase");
+const symbolsEl = document.getElementById("symbols");
+const resultEl = document.getElementById("result");
+const generateEl = document.getElementById("generate");
+const clipboardEl = document.getElementById("clipboard");
+
+const randomFunc = {
+  lower: getRandomLower,
+  upper: getRandomUpper,
+  number: getRandomNumber,
+  symbol: getRandomSymbol
+};
+
+generateEl.addEventListener("click", () => {
+  const length = +lengthEl.Value;
+  const hasLower = lowercaseEl.checked;
+  const hasUpper = UppercaseEl.checked;
+  const hasNumber = numberEl.checked;
+  const hasSymbol = symbolsEl.checked;
+
+  console.log(hasLower);
+});
+
 //Generator functions
 
 function getRandomLower() {
@@ -8,6 +35,19 @@ function getRandomUpper() {
   return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
 }
 
+function getRandomNumber() {
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+
+function getRandomSymbol() {
+  const symbols = "!@#$%^&*(){}[]=<>/,.";
+  return symbols[Math.floor(Math.random() * symbols.length)];
+}
+
 console.log(getRandomLower());
 
 console.log(getRandomUpper());
+
+console.log(getRandomNumber());
+
+console.log(getRandomSymbol());
