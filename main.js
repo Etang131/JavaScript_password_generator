91b154276defa4929a0e414f7fea0afd66307a39
Erik Tangvik <erikjt@outlook.com>
2019-10-22 09:47:46 -0500

    Worked on generatePassword function / fixed some errors

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 // Dom elements
 const resultEl = document.getElementById("result");
 const lengthEl = document.getElementById("length");
-const uppercaseEl = document.getElementById("upper");
+const uppercaseEl = document.getElementById("uppercase");
 const lowercaseEl = document.getElementById("lowercase");
+const numbersEl = document.getElementById("numbers");
 const symbolsEl = document.getElementById("symbols");
-const resultEl = document.getElementById("result");
 const generateEl = document.getElementById("generate");
 const clipboardEl = document.getElementById("clipboard");
 
@@ -15,17 +15,37 @@ const randomFunc = {
   symbol: getRandomSymbol
 };
 
+//Generate event listen
 generateEl.addEventListener("click", () => {
   const length = +lengthEl.Value;
   const hasLower = lowercaseEl.checked;
-  const hasUpper = UppercaseEl.checked;
-  const hasNumber = numberEl.checked;
+  const hasUpper = uppercaseEl.checked;
+  const hasNumber = numbersEl.checked;
   const hasSymbol = symbolsEl.checked;
 
-  console.log(hasLower);
+  resultEl.innerText = generatePassword(
+    hasLower,
+    hasUpper,
+    hasNumber,
+    hasSymbol,
+    length
+  );
 });
 
-//Generator functions
+//Generate password function
+function generatePassword(lower, upper, number, symbol, length) {
+  //1. Init password var
+  //2. Fliter out unchecked types
+  //3. loop over length call generator function for each type
+  //4. add final password to the password var and return
+
+  let generatedPassword = "";
+
+  const typesCount = lower + upper + number + symbol;
+  console.log("typesCount: ", typesCount);
+}
+
+//Generator functions  www.net-comber.com/charset.html
 
 function getRandomLower() {
   return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
@@ -43,11 +63,3 @@ function getRandomSymbol() {
   const symbols = "!@#$%^&*(){}[]=<>/,.";
   return symbols[Math.floor(Math.random() * symbols.length)];
 }
-
-console.log(getRandomLower());
-
-console.log(getRandomUpper());
-
-console.log(getRandomNumber());
-
-console.log(getRandomSymbol());
